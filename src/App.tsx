e838a5d8c018a7421e8ccb8945f1868fe36ce4c5
jay salidwong <wansanohj@gmail.com>
2020-11-30 17:03:15 +0700

    [frontend] test connect firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import firebase from 'firebase'
+import { firebaseConfig } from './firebase/config'
+
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig)
 
 const App: React.FC = () => {
+    const database = firebase.database()
+
+    // eslint-disable-next-line no-unused-vars
+    const [user, setUser] = useState()
+    const [input, setInput] = useState('')
+    const [email] = useState('test@gmail.com')
+    const [password] = useState('123456')
+
+    useEffect(() => {
+        const ref = database.ref('user')
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then((user) => {
+                console.log('login', user)
+            })
+            .catch((error) => {
+                console.log('error', error)
+            })
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                console.log('user sign in', user.uid)
+            } else {
+                console.log('user sign out')
+            }
+        })
+
+        ref.on('value', (snapshot) => setUser(snapshot.val()))
+    }, [])
+
+    const handleInputChange = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ): void => {
+        setInput(e.target.value)
+    }
+    const handleButtonClick = (): void => {
+        const userRef = database.ref('user')
+
+        userRef.update({ test: { firstName: 'testttt' } })
+    }
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <input value={input} onChange={handleInputChange} />
+            <button onClick={handleButtonClick}>click</button>
         </div>
     )
 }
---
+++ src/firebase/config.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: 'AIzaSyBs0MhclCN4w_9e8KbCR2kmfrBWSZ4rAGg',
+    authDomain: 'tit-tang.firebaseapp.com',
+    databaseURL: 'https://tit-tang.firebaseio.com',
+    projectId: 'tit-tang',
+    storageBucket: 'tit-tang.appspot.com',
+    messagingSenderId: '708546722063',
+    appId: '1:708546722063:web:94d489f970d070003e667a',
+    measurementId: 'G-2YZ9RZD4MM',
+}
